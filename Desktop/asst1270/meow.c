695a4a8e8493b421255c919bcf3ffd2f74d1a490
AliiceK <112477235+AliiceK@users.noreply.github.com>
2022-10-06 14:35:15 +0300

    hammingdistance

--- Desktop/asst1270/meow.c
+++ Desktop/asst1270/meow.c
@@ -4,5 +4,18 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-    printf("meow");
+    puts("enter 2 bits to calculate their hamming distance");
+    int bit1;
+    int bit2;
+    scanf("%d", &bit1);
+    scanf("%d", &bit2);
+    int c = bit1 ^ bit2;
+    // printf("%d", c);
+    int hammingdistance = 0;
+    while (c > 0) {
+        hammingdistance += c % 2;
+        c /= 2;
+    }
+    printf("The hamming distance between %d and %d is %d", bit1, bit2, hammingdistance);
+
 }
