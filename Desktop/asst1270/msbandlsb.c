28521471dd22d525ea348024aa8151a8bd7101b2
AliiceK <112477235+AliiceK@users.noreply.github.com>
2022-10-09 22:09:37 +0300

    finalized msbandlsb

--- Desktop/asst1270/msbandlsb.c
+++ Desktop/asst1270/msbandlsb.c
@@ -7,19 +7,23 @@ int msb();
 int main() {
     //10100 = 40 
     int leastsig = lsb(40);
-  //  int mostsig = msb(40);
-    printf("%d", leastsig);
-  //  printf("%d", mostsig);
+    int mostsig = msb(40);
+    printf("%d\n", leastsig);
+    printf("%d\n", mostsig);
+    printf("the difference between the msb and lsb is %d", mostsig - leastsig);
 
 }
 
-int msb(int num) {
-    int position = -1;
-    while (!(num & 1)) {
-        num <<= 1;
-        position++;
+int msb(unsigned int num)
+{
+    int cnt = 0;
+
+    while (num) {
+        cnt++;
+        num = num >> 1;
     }
-    return position;
+
+    return cnt - 1;
 }
 
 int lsb(int num) {
